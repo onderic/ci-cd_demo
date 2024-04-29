d2a8aa98f06de4dc6c31cfea3eb1c1cd7b20d6b4
onderic <ondericollins80@gmail.com>
2024-04-29 18:43:50 +0300

    add other test cases

--- udms/tests/test_models.py
+++ udms/tests/test_models.py
@@ -15,3 +15,19 @@ class DisciplinaryCaseTestModel(TestCase):
         case = DisciplinaryCase.objects.get(id=1)
         field_label = case._meta.get_field('description').verbose_name
         self.assertEqual(field_label, 'description')
+
+    def test_created_at_auto_now_add(self):
+        case = DisciplinaryCase.objects.get(id=1)
+        self.assertIsNotNone(case.created_at)
+      
+
+    def test_updated_at_auto_now(self):
+        case = DisciplinaryCase.objects.get(id=1)
+        # Simulate update
+        case.title = 'Updated Title'
+        case.save()
+        updated_case = DisciplinaryCase.objects.get(id=1)
+        self.assertNotEqual(case.created_at, updated_case.updated_at)
+        # ^ Changed from case.created_at to case.updated_at
+        self.assertEqual(case.updated_at, updated_case.updated_at)
+      

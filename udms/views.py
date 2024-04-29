b5e67e08793333f8a06c0d23a9706237d728fea4
onderic <ondericollins80@gmail.com>
2024-04-29 19:12:40 +0300

    setup template

--- udms/views.py
+++ udms/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def dashboard(request):
+    context = {
+        'message': 'Welcome to the University Disciplinary Management System!'
+    }
+    return render(request, 'dashboard.html', context)

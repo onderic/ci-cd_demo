e6cbaf0c7a327c9ca2cd8fdef32514361b4738e7
onderic <ondericollins80@gmail.com>
2024-04-30 18:56:57 +0300

    fixes

--- udms/views.py
+++ udms/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 def dashboard(request):
     context = {
-        'message': 'Welcome to the University Disciplinary Management System!'
+        'message': 'Welcome to the University Disciplinary Management System!..'
     }
     return render(request, 'dashboard.html', context)
